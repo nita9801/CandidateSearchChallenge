533cd84c69b86b881c10a2c42b60591da07e4fd3
Christine Lopez <mrsChristinemlopez@gmail.com>
2025-02-24 15:22:07 -0500

    fixing config syntax error

--- Develop/vite.config.ts
+++ Develop/vite.config.ts
@@ -4,5 +4,14 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   envDir: './env',
-  plugins: [react()],
+  plugins: [react()], 
+  // other rules...
+  parserOptions: {
+  ecmaVersion: 'latest',
+  sourceType: 'module',
+  project: ['./tsconfig.json', './tsconfig.node.json'],
+  tsconfigRootDir: __dirname,},
 });
+
+ 
+
